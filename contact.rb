5125a4f2729dd51364fc605ab905aa9b958d387e
jeffrothwell <rothwelljeff@gmail.com>
2017-12-20 14:59:54 -0500

    write update method for contact

--- contact.rb
+++ contact.rb
@@ -41,8 +41,23 @@ class Contact
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
-
+  def update(attribute, new_value)
+    # I believe this will allow me to access the writer
+    # for the attribute, and run this method on the self
+    # object on which the update method is called
+    # never mind, it won't work, it will literally
+    # read this and try to run some .attribute method
+    # Dan suggested looking up the send method
+    # self.attribute = new_value
+    if attribute == "first_name"
+      self.first_name = new_value
+    elsif attribute == "last_name"
+      self.last_name = new_value
+    elsif attribute == "email"
+      self.email = new_value
+    elsif attribute == "notes"
+      self.notes = new_value
+    end
   end
 
   # This method should work similarly to the find method above
@@ -55,17 +70,19 @@ class Contact
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts = []
   end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-
+  def delete(contact)
+    # using the .delete method with contact as an argument
+    # returns the deleted object itslef, or nil if non existent
+    @@contacts.delete(contact)
   end
 
   # Feel free to add other methods here, if you need them.
